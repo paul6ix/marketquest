7b46f51ee529312cef5f733dc1870223a516c501
Paul6ix <okporp@gmail.com>
2016-08-04 11:16:13 +0100

    working contact us form

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
 $name = $_POST['name'];
 $email = $_POST['mail'];
 $message = $_POST['comment'];
-$to = "okporp@gmail.com";
+$to = " info@marketquest.ng";
 $subject = "New Message";
 mail($to, $subject, $message, "from " . $name);
 echo "Your Message have been sent successfully";
